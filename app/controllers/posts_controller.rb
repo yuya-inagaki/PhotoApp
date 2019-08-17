cc91fabac76088179b950ce6b275820f8e94a038
Yuya Inagaki <yuya1756@live.jp>
2019-08-17 14:23:59 +0900

    N+1問題の解決

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    # 記事一覧を取得する際にユーザーも取得することでN+1問題を解決する
+    @posts = Post.all.includes(:user)
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
